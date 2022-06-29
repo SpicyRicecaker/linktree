f070c43b018534dd897839e0875062f6fe6f9d7f
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2022-06-29 03:14:14 -0700

    initial component portion of tut

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,6 @@
+export function HelloWorld () {
+    const name = "Hello ";
+    const style = { "background-color": "#2c4f7c", color: "#FFF" };
+    
+    return <div style={style}>{name} World!</div>
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import './index.css';
+import App from './App';
+// import { HelloWorld } from './HelloWorld';
+
+// render(() => <HelloWorld />, document.getElementById('root') as HTMLElement);
+render(() => <App />, document.getElementById('root') as HTMLElement);
